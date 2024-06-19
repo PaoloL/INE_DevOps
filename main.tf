82fc7100e652f0214679f8d79504f222ced8831e
Paolo Latella <maverick@Paolos-MacBook-Pro.local>
2024-06-19 09:11:30 +0200

    Fixed ECR name

--- main.tf
+++ main.tf
@@ -2,14 +2,26 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_ecr_repository" "my_repository" {
-  name                 = "${var.owner}-registry"
+resource "aws_ecr_repository" "my_repository_bikes" {
+  name                 = "${var.owner}-bikes_repo"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
   }
 }
 
-output "repository_url" {
-  value = aws_ecr_repository.my_repository.repository_url
+resource "aws_ecr_repository" "my_repository_cars" {
+  name                 = "${var.owner}-cars_repo"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+output "bikes_repository_url" {
+  value = aws_ecr_repository.my_repository_bikes.repository_url
+}
+
+output "cars_repository_url" {
+  value = aws_ecr_repository.my_repository_cars.repository_url
 }
